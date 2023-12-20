f2927f19f1aaeba7e39b4326906b58b3e0bde2d8
DonConcha <150167557+DonConcha@users.noreply.github.com>
2023-12-19 18:42:45 -0600

    updated added start & submit button

--- index.js
+++ index.js
@@ -1,65 +1,30 @@
-var questions = [
-    {
-        text: "how do you copy an Object in javascript?",
-        answers: [
-            "Copy & Paste ",
-            "Object.assign()",
-            "Using spread parameter", 
-        ],
-        correctIndex: 1
-    },
-    {
-      text: "How do you bla bla bla",
-      answers: ["a", "b", "c"],
-      correctIndex: 0,
-    },
-    {
-        text: "What is asldkjsdkj?",
-        answers: ["a", "b", "c"],
-        correctIndex: 0,
-      },
-      {
-        text: "When is this project due?",
-        answers: ["a", "b", "c"],
-        correctIndex: 0,
-      },
-      {
-        text: "How do you bla bla bla",
-        answers: ["a", "b", "c"],
-        correctIndex: 0,
-      },
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>High Scores </title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
 
-];
+<body>
+   <div class="container">
+      <header>
+         <div id="timeleft">75</div>
+      </header>
+      <section id="start">
+         <p>Welcome to the coding quiz!</p> 
+         <button id="btnStartQuiz">Start Quiz</button>
+      </section>
+    </div>
 
-//seclectors
-var startBtn = document.querySelector("#btnStartQuiz");
-var submitBtn = document.querySelector("#btnSubmitAnswer");
-var timeLeft = document.querySelector("#timeleft");
+  <section id="quiz">
+     <div id="question-text"></div>
+     <ul id="question-answers"></ul>
+     <button id="btnSubmitAnswer">Submit</button>
+  </section>
 
-// Initial Values 
-var totalTimeLeft = 75;
-var pointsOff = 10;
-var questionIndex = 0;
-
- function displayTimeLeft() {
-    totalTimeLeft--;
-    timeLeft.textContent = totalTimeLeft;
- }
-
- function displayNextQuestion() {
-   var question = questions[questionIndex];
-   questionText.textContent = question.text;
-
-   for (var i = 0 ; i < question.answers; i++) {
-       var answers = question.answers[i];
-       var liEl = document.createElement('li');
-   }
- }
-
-
-function startQuiz() {
-  setTimeout(function() {
-    displayTimeLeft();
-
-  }, 1000);
-}
+    <script src=".assets/javascript/high-scores.js"></script>
+</body>
+  
+</html>
